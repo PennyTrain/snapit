2e473e9cb95d50aebf6cdcd162c41763867d9ab7
Penny Train <penny.train04@gmail.com>
2024-05-05 11:54:04 +0100

    Installed bootstrap, and add component NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <NavBar />
     </div>
   );
 }
