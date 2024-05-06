59c988e2c90e52c2a25867f064003e7dc917f19a
Penny Train <penny.train04@gmail.com>
2024-05-06 14:44:45 +0100

    Installed react-router, set up Navbar
    Navbar now shows different displays when the tab is clicked, without refreshing the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import { Container } from 'react-bootstrap';
+import { Route, Switch } from "react-router-dom";
+
+// The Switch holds all our Routes,  and renders a given component when  
+// a Route path matches the current URL. The render prop on our Route component  
+// accepts a function that returns a component to  be rendered when the Route path is matched.
+// The path prop is the browser url  the user will be at when they see  
+// the component in our render prop. The “exact” prop tells the route to  
+// only render its component when the  url entered is exactly the same.
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Container className="Down">
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path="/login" render={() => <h1>Log in</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path="/register" render={() => <h1>Register</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,30 @@
 import React from 'react'
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css"
+import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
   return (
     <div>
       <Navbar className={styles.NavBar} bg="light" expand="lg">
         <Container>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
+          <NavLink to="/">
+            <Navbar.Brand>
+              {/* add back src={logo} */}
+              <img alt="logo" height="45" />
+            </Navbar.Brand>
+          </NavLink>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <NavLink exact to="/" activeClassName={styles.Active} className={styles.NavLink}>Home</NavLink>
+              <NavLink to="/login" activeClassName={styles.Active} className={styles.NavLink}>Sign in</NavLink>
+              <NavLink to="/register" activeClassName={styles.Active} className={styles.NavLink}>Sign up</NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   )
 }
