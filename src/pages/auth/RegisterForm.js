3dd69439afef42eeb4c444ad5fe3cee017262bc8
Penny Train <penny.train04@gmail.com>
2024-05-13 15:06:37 +0100

    Edit RegisterForm.js, now can register new users and send information to the API

--- src/pages/auth/RegisterForm.js
+++ src/pages/auth/RegisterForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from "react-bootstrap";
 import { Link, useHistory } from 'react-router-dom';
-import axios from 'axios';
+import axios from "axios";
 
 const RegisterForm = () => {
     const [registerData, setRegisterData] = useState({
@@ -9,8 +9,8 @@ const RegisterForm = () => {
         password1: "",
         password2: "",
     })
-    const {username, password1, password2} = registerData;
-    const history= useHistory()
+    const { username, password1, password2 } = registerData;
+    const history = useHistory()
     const [errors, setErrors] = useState({});
 
 
@@ -41,31 +41,31 @@ const RegisterForm = () => {
             <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="username">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control onChange={handleChange} value={username} type="text" placeholder="username" />
+                    <Form.Control name="username" onChange={handleChange} value={username} type="text" placeholder="username" />
                 </Form.Group>
-                {errors.password1?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
+                {errors.username && (
+                    <Alert variant="warning">
+                        {errors.username}
+                    </Alert>
+                )}
                 <Form.Group controlId="password1">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control onChange={handleChange} value={password1} type="password" placeholder="Password" />
+                    <Form.Control name="password1" onChange={handleChange} value={password1} type="password" placeholder="Password" />
                 </Form.Group>
-                {errors.password1?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
+                {errors.password1 && (
+                    <Alert variant="warning">
+                        {errors.password1}
+                    </Alert>
+                )}
                 <Form.Group controlId="password2">
                     <Form.Label>Please Confirm Your Password</Form.Label>
-                    <Form.Control onChange={handleChange} value={password2} type="password" placeholder="Password Confirmation" />
+                    <Form.Control name="password2" onChange={handleChange} value={password2} type="password" placeholder="Password Confirmation" />
                 </Form.Group>
-                {errors.password1?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
+                {errors.password2 && (
+                    <Alert variant="warning">
+                        {errors.password2}
+                    </Alert>
+                )}
                 <Button variant="primary" type="submit">
                     Sign Up
                 </Button>
